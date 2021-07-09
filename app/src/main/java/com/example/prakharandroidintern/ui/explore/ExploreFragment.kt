f356ce2c0c541e15f2cd076635c8235e33fed922
Prakhar <prakhar@everything.net.in>
2021-07-09 09:39:02 +0530

    implemented login

---
+++ app/src/main/java/com/example/prakharandroidintern/ui/explore/ExploreFragment.kt
@@ -0,0 +1,39 @@
+package com.example.prakharandroidintern.ui.explore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.prakharandroidintern.databinding.FragmentExploreBinding
+class ExploreFragment : Fragment() {
+
+    private var _binding: FragmentExploreBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentExploreBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = "Explore"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
